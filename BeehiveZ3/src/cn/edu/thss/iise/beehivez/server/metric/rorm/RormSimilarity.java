ee79c89d8cf38dffd68346b2fe1d494dc2418c62
Shuhao Wang <shudiwsh2009@gmail.com>
2015-06-20 15:22:11 +0800

    modify descriptions

--- BeehiveZ3/src/cn/edu/thss/iise/beehivez/server/metric/rorm/RormSimilarity.java
+++ BeehiveZ3/src/cn/edu/thss/iise/beehivez/server/metric/rorm/RormSimilarity.java
@@ -1,5 +1,5 @@
 /**
- * Behavioral Petri Net Similarity Algorithm based on Shortest Synchronization Distance
+ * Behavioral Petri Net Similarity Algorithm based on the Matrix of Refined Ordering Relations with Uncertainty
  */
 package cn.edu.thss.iise.beehivez.server.metric.rorm;
 
@@ -88,7 +88,7 @@ public class RormSimilarity extends PetriNetSimilarity {
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return "Extended Causal Footprint Matrix Similarity";
+		return "RORM Simialrity";
 	}
 
 	/*
@@ -101,7 +101,7 @@ public class RormSimilarity extends PetriNetSimilarity {
 	@Override
 	public String getDesription() {
 		// TODO Auto-generated method stub
-		return "Behavioral Petri Net Similarity Algorithm based on Extended Causal Footprint Matrix Similarity, which is computed on Complete Finite Prefix.";
+		return "Behavioral Petri Net Similarity Algorithm based on the Matrix of Refined Ordering Realtions with Uncertainty.";
 	}
 
 	public static void main(String[] args) throws Exception {
