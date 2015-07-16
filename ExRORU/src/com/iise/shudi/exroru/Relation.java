55638ea544db860da511757c37c74ec68b38f5dd
Shuhao Wang <shudiwsh2009@gmail.com>
2015-07-16 23:59:51 +0800

    adjust structure and remove ExRORU

--- ExRORU/src/com/iise/shudi/exroru/Relation.java
+++
@@ -1,5 +0,0 @@
-package com.iise.shudi.exroru;
-
-public enum Relation {
-    ALWAYS, SOMETIMES, NEVER
-}
--- ExRORU/src/org/jbpt/petri/unfolding/BranchingProcess.java
+++
@@ -1,7 +0,0 @@
-package org.jbpt.petri.unfolding;
-
-import org.jbpt.petri.*;
-
-public class BranchingProcess extends
-        AbstractBranchingProcess<BPNode, Condition, Event, Flow, Node, Place, Transition, Marking> {
-}
